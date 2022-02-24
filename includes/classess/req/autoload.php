31e09852957a718fc3b706ee94b1af68364a56f6
T0M4S <83116688+isnocode@users.noreply.github.com>
2022-02-24 15:52:56 +0700

    Add files via upload

---
+++ includes/classess/req/autoload.php
@@ -0,0 +1,14 @@
+<?php
+/*  ---------------------------------------------------------------------------
+ * 	@package	: Autoload : Including Required Files
+ *	@author 	: Invoice Generator
+ *	@version	: 1.0
+ *	@link		: #
+ *	@copyright	: Copyright (c) 2019
+ *	--------------------------------------------------------------------------- */
+
+require('fpdf/tfpdf.php');
+
+require('rotation/rotation.php');
+
+?>
